6e6791ac94709aad51f54a0667cacfed4c9c97ef
Ji Yim <gsooyim@gmail.com>
2018-04-14 19:33:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,12 @@
 def square_array(array)
+  new_numbers = []
+  array.each do |number|
+    new_numbers.push(number**2)
+  end
+  new_numbers
+end
+
+def square_array(array)
   squared_array = []
   array.each { |number| squared_array << number**2}
   squared_array
